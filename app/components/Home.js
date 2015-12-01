b7baac6cdcdcc318f7c668b83fb61156b943a4d0
Zachary Lee <w.zachary.lee@gmail.com>
2015-11-30 21:19:34 -0800

    bootstrapped

---
+++ app/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>HELLO FROM HOME</h3>
+      </div>
+      )
+  }
+}
+
+export default Home;
